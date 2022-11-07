606631354455d266ace9b768256d7f67c40b9461
Shane McGuirt <10144278+srmcguirt@users.noreply.github.com>
2022-11-07 13:05:30 -0500

    :rocket: Initial commit

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,10 @@
+require('@rushstack/eslint-config/patch/modern-module-resolution');
+
+module.exports = {
+  extends: [
+    "@rushstack/eslint-config/profile/node",
+    "turbo",
+    "prettier"
+  ],
+  parserOptions: { tsconfigRootDir: __dirname }
+};
